8fe02f0093dc01851393eea0c69a265dff317d93
Jason Weyland <jason.weyland13@gmail.com>
2020-05-01 17:07:33 -0400

    about us

---
+++ js.js
@@ -0,0 +1,23 @@
+$(function () {
+    $("#btnAbout").click(function () {
+        $("#aboutTab").addClass("active show");
+    });
+    $("#btnMeet").click(function () {
+        $("#officerTab").addClass("show active");
+    });
+    $("#btnFaq").click(function () {
+        $("#faqTab").addClass("show active");
+    });
+    $("#btnMember").click(function () {
+        $("#joinTab").addClass("show active");
+    });
+});
+
+// btnAbout
+// btnMeet
+// btnFaq
+// btnMember
+// aboutTab
+// officerTab
+// faqTab
+// joinTab
