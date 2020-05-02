56dca62bec01ef96e4ac73c0cd1e4b3a1a346845
Jason Weyland <jason.weyland13@gmail.com>
2020-05-02 11:46:26 -0400

    added scss, starting nav tabs

--- js.js
+++ js.js
@@ -1,16 +1,22 @@
-$(function () {
-    $("#btnAbout").click(function () {
-        $("#aboutTab").addClass("active show");
-    });
-    $("#btnMeet").click(function () {
-        $("#officerTab").addClass("show active");
-    });
-    $("#btnFaq").click(function () {
-        $("#faqTab").addClass("show active");
-    });
-    $("#btnMember").click(function () {
-        $("#joinTab").addClass("show active");
-    });
+// $(function () {
+//     $("#btnAbout").click(function () {
+//         $("#aboutTab").addClass("active show");
+//     });
+//     $("#btnMeet").click(function () {
+//         $("#officerTab").addClass("show active");
+//     });
+//     $("#btnFaq").click(function () {
+//         $("#faqTab").addClass("show active");
+//     });
+//     $("#btnMember").click(function () {
+//         $("#joinTab").addClass("show active");
+//     });
+// });
+
+var image = document.getElementsByClassId("flowStone");
+new simpleParallax(image, {
+    scale: 1.5,
+    orientation: "down",
 });
 
 // btnAbout
