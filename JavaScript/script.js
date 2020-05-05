cf2f94c6a2cab51e093526868ca90e387f2bb88f
Jason Weyland <jason.weyland13@gmail.com>
2020-05-04 21:40:26 -0400

    become a member page

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,4 @@
+// Dropdown Menu
 $(function () {
     $("#dropMeet").click(function () {
         if ($("#linkAbout").hasClass("active")) {
@@ -98,6 +99,7 @@ $(function () {
     });
 });
 
+// nav join
 $(function () {
     $("#navJoin").click(function () {
         if ($("#linkMeet").hasClass("active")) {
@@ -123,50 +125,82 @@ $(function () {
     });
 });
 
-// $(function () {
-//     $(".card-deck")
-//         .children("#presidentCard")
-//         .mouseover(function () {
-//             $("#presidentCard").removeClass("border-succeses");
-//             $("#presidentCard").addClass("border-warning");
-//         });
-// });
+// footer faq
+$(function () {
+    $("#footerFaq").click(function () {
+        if ($("#linkMeet").hasClass("active")) {
+            $("#tabMeet").removeClass("show active");
+            $("#linkMeet").removeClass("active");
+            $("#linkFaq").addClass("active");
+            $("#tabFaq").addClass("show active");
+        } else {
+            if ($("#linkJoin").hasClass("active")) {
+                $("#tabJoin").removeClass("show active");
+                $("#linkJoin").removeClass("active");
+                $("#linkFaq").addClass("active");
+                $("#tabFaq").addClass("show active");
+            } else {
+                if ($("#linkAbout").hasClass("active")) {
+                    $("#tabAbout").removeClass("show active");
+                    $("#linkAbout").removeClass("active");
+                    $("#linkFaq").addClass("active");
+                    $("#tabFaq").addClass("show active");
+                }
+            }
+        }
+    });
+});
 
-// $(function () {
-//     $("#dropMeet").click(function () {
-//         $("#linkAbout").removeClass("active");
-//         $("#linkMeet").addClass("active");
-//         $("#tabAbout").removeClass("show active");
-//         $("#tabMeet").addClass("show active");
-//     });
-// });
+// Footer Join
+$(function () {
+    $("#footerJoin").click(function () {
+        if ($("#linkMeet").hasClass("active")) {
+            $("#tabMeet").removeClass("show active");
+            $("#linkMeet").removeClass("active");
+            $("#linkJoin").addClass("active");
+            $("#tabJoin").addClass("show active");
+        } else {
+            if ($("#linkFaq").hasClass("active")) {
+                $("#tabFaq").removeClass("show active");
+                $("#linkFaq").removeClass("active");
+                $("#linkJoin").addClass("active");
+                $("#tabJoin").addClass("show active");
+            } else {
+                if ($("#linkAbout").hasClass("active")) {
+                    $("#tabAbout").removeClass("show active");
+                    $("#linkAbout").removeClass("active");
+                    $("#linkJoin").addClass("active");
+                    $("#tabJoin").addClass("show active");
+                }
+            }
+        }
+    });
+});
 
-// $(function () {
-//     $("#btnAbout").click(function () {
-//         $("#aboutTab").addClass("active show");
-//     });
-//     $("#btnMeet").click(function () {
-//         $("#officerTab").addClass("show active");
-//     });
-//     $("#btnFaq").click(function () {
-//         $("#faqTab").addClass("show active");
-//     });
-//     $("#btnMember").click(function () {
-//         $("#joinTab").addClass("show active");
-//     });
-// });
-
-// var image = document.getElementsByClassId("flowStone");
-// new simpleParallax(image, {
-//     scale: 1.5,
-//     orientation: "down",
-// });
-
-// btnAbout
-// btnMeet
-// btnFaq
-// btnMember
-// aboutTab
-// officerTab
-// faqTab
-// joinTab
+// paypal
+paypal
+    .Buttons({
+        style: {
+            shape: "rect",
+            color: "blue",
+            layout: "vertical",
+            label: "paypal",
+        },
+        createOrder: function (data, actions) {
+            return actions.order.create({
+                purchase_units: [
+                    {
+                        amount: {
+                            value: "15",
+                        },
+                    },
+                ],
+            });
+        },
+        onApprove: function (data, actions) {
+            return actions.order.capture().then(function (details) {
+                alert("Transaction completed by " + details.payer.name.given_name + "!");
+            });
+        },
+    })
+    .render("#paypal-button-container");
